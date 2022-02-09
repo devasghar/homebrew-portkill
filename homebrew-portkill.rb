e435b63b8307c5460d46066cb7a37a044bfd0e8f
devasghar <dev.asghar@gmail.com>
2022-02-10 01:13:42 +0500

    [Updated] - updated portkill ruby file

--- homebrew-portkill.rb
+++ homebrew-portkill.rb
@@ -1,13 +1,14 @@
 class HomebrewPortkill < Formula
-  desc "Brew formula for killing ports"
+  desc "Brew tab for killing ports"
   homepage "https://github.com/devasghar/homebrew-portkill"
-  url "https://github.com/devasghar/homebrew-portkill/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "856649a04ded2126235e64fa1e4b8ab7fd3d48b465db35a308f819c8cafb0711"
+  url "https://github.com/devasghar/homebrew-portkill/archive/refs/tags/v0.0.5.tar.gz"
+  sha256 "67a701d9d71281633403f59eb279a5b6721f8671f523bf4809336a229dc859c2"
   license "MIT"
+
   depends_on "lsof"
 
   def install
-     bin.install 'portkill'
+  bin.install 'portkill'
   end
 
   test do
