43cea90daf118b8d46be1e22159b8c790d2821d8
devasghar <dev.asghar@gmail.com>
2022-02-10 00:54:02 +0500

    [Homebrew] - Ruby file added

--- homebrew-portkill.rb
+++ homebrew-portkill.rb
@@ -1,35 +1,16 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class HomebrewPortkill < Formula
   desc "Brew formula for killing ports"
-  homepage ""
+  homepage "https://github.com/devasghar/homebrew-portkill"
   url "https://github.com/devasghar/homebrew-portkill/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "856649a04ded2126235e64fa1e4b8ab7fd3d48b465db35a308f819c8cafb0711"
   license "MIT"
-
-  # depends_on "cmake" => :build
   depends_on lsof
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
      bin.install 'portkill'
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test homebrew-portkill`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
