c1a6db40860195f8114a5d408d025faad95a94a4
devasghar <dev.asghar@gmail.com>
2022-02-10 00:58:23 +0500

    [Homebrew] - Ruby file added

--- homebrew-portkill.rb
+++ homebrew-portkill.rb
@@ -4,7 +4,7 @@ class HomebrewPortkill < Formula
   url "https://github.com/devasghar/homebrew-portkill/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "856649a04ded2126235e64fa1e4b8ab7fd3d48b465db35a308f819c8cafb0711"
   license "MIT"
-  depends_on lsof
+  depends_on "lsof"
 
   def install
      bin.install 'portkill'
